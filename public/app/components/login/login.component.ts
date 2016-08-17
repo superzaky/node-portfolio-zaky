25fc799761b4cd3c8b13e563173f9037c36867fd
superzaky <superkytoz@gmail.com>
2016-08-17 15:10:08 +0200

    Added toasts

--- public/app/components/login/login.component.ts
+++ public/app/components/login/login.component.ts
@@ -1,27 +1,46 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
+import {ToasterContainerComponent, ToasterService, ToasterConfig} from 'angular2-toaster/angular2-toaster';
 
 @Component({
     moduleId: module.id,
     selector: 'login',
+    directives: [ToasterContainerComponent],
     templateUrl: 'login.component.html',
-    providers: [UserService]
+    providers: [UserService, ToasterService]
 })
 
 export class LoginComponent {
     user: User = new User();
-    loginRes: String
-    constructor(private _userService: UserService) {
+    loginRes: String;
+    private toasterService: ToasterService;
 
+    public toasterconfig: ToasterConfig = new ToasterConfig({
+        showCloseButton: true,
+        tapToDismiss: false,
+        timeout: 0
+    });
+    
+    constructor(private _userService: UserService, toasterService: ToasterService) {
+        this.toasterService = toasterService;
     }
+
     data = {};
     onSubmit() {
         this._userService.login(this.user)
-            .subscribe(res => {
-                console.log("res onSubmit");
-                console.log(res);
-                this.loginRes = res;
-            });
+            .subscribe(
+                //we are using an arrow function here
+                res => {
+                    console.log("res onSubmit");
+                    console.log(res);
+                            
+                        this.toasterService.pop('success', 'Success', 'You have logged in ' + res.username);
+                    
+                },
+                error => {
+                     this.toasterService.pop('error', 'Failed', error._body);
+                }
+            );
     }
 }
--- public/app/services/user.service.ts
+++ public/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Headers, RequestOptions, Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable }     from  'rxjs/Rx';;
 import { User } from '../models/user';
 
 @Injectable()
@@ -12,7 +14,6 @@ export class UserService {
     }
 
     login(user: User) {
-//        this.loginUrl = window.location.origin + "/api/auth/login"; 
         this.loginUrl = "http://localhost:3000/api/auth/login";
         let data = { "username": user.username, "password": user.password };
         let body = JSON.stringify(data);
@@ -20,11 +21,17 @@ export class UserService {
         let options = new RequestOptions({ headers: headers });
 
         return this._http.post(this.loginUrl, body, options)
-            .map(this.extractData);
+            .map(this.extractData)
+            .catch(this.handleError);
     }
 
     private extractData(res: Response) {
         let body = res.json();
         return body || {};
     } 
+    
+    private handleError(error: any) {
+        //the Observable catches and throws an error
+        return Observable.throw(error.message || error);
+    }
 }
