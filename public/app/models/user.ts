288312d7d165c8c2e0f18f62e02391003256174d
superzaky <superkytoz@gmail.com>
2017-09-26 19:50:32 +0200

    Upgrade to Angular 4.

--- public/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    username: string;
-    password: string;
-
-    constructor(values: Object = {}) {
-        Object.assign(this, values);
-    }
-}
--- public/app/src/user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../../app/models/user';
-describe('User', () => {
-    it('has username', () => {
-        let user: User = { username: 'marco', password: 'open' };
-        expect(user.username).toEqual('marco');
-    });
-});
