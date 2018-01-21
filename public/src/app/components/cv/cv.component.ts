b7f2c3122db9a8946d52f6ca1d15b932309a52fd
superzaky <superkytoz@gmail.com>
2018-01-21 22:41:46 +0100

    Add class when in view.

--- public/src/app/components/cv/cv.component.ts
+++ public/src/app/components/cv/cv.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core'
 
 @Component({
     moduleId: module.id,
@@ -7,7 +7,37 @@ import { Component } from '@angular/core';
 })
 
 export class CvComponent { 
+    items: any;
+
     constructor() {
-        
+    
+    }
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        this.items = document.querySelectorAll(".timeline li");
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll($event) {
+        // code for the isElementInViewport function
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.isElementInViewport(this.items[i])) {
+            this.items[i].classList.add("in-view");
+            }
+        }
+        // window.addEventListener("load", this.callbackFunc);
+        // window.addEventListener("scroll", this.callbackFunc);
+    }
+
+    isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
     }
 }
